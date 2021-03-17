855ab426b47f124f9c8bc0fa55bb04e528577fa9
joelgrod <joelgrod@gmail.com>
2021-03-17 11:43:18 +0000

    Basic template form example finished, dinamics template form example finished

--- src/app/template/pages/basics/basics.component.ts
+++ src/app/template/pages/basics/basics.component.ts
@@ -1,6 +1,12 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface BasicForm {
+  product_name: string;
+  product_price: number;
+  product_stock: number;
+}
+
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
@@ -10,6 +16,12 @@ import { NgForm } from '@angular/forms';
 export class BasicsComponent {
 
   @ViewChild('basicsform') form!: NgForm;
+
+  init_form: BasicForm = {
+    product_name: '',
+    product_price: 0,
+    product_stock: 0
+  }
 
   validate_name(): boolean {
     return this.form?.controls.product_name?.invalid &&
@@ -28,6 +40,8 @@ export class BasicsComponent {
 
   save(): void {
     console.log(this.form);
+    // Reset form
+    this.form.resetForm(this.init_form);
   }
 
 }
--- src/app/template/pages/dinamics/dinamics.component.ts
+++ src/app/template/pages/dinamics/dinamics.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Person {
+  name: string;
+  favourites: Favourite[];
+}
+
+interface Favourite {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dinamics',
@@ -6,11 +16,35 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public person: Person = {
+    name: 'Test',
+    favourites: [
+      {id: 1, name: 'Baba is you'},
+      {id: 2, name: 'Oxygen Not Included'},
+      {id: 3, name: 'Loop Hero'},
+    ]
   }
 
+  public new_favourite: string = '';
+
+  add(): void {
+    const new_favourite: Favourite = {
+      id: this.person.favourites.length + 1,
+      name: this.new_favourite
+    };
+
+    this.person.favourites.push({...new_favourite});
+    this.new_favourite = '';
+  }
+
+  delete(index: number): void {
+    this.person.favourites.splice(index, 1);
+  }
+
+  save(): void {
+    console.log('Form saved');
+  }
+  
 }
