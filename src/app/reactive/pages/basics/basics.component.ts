54b121429af5c7f781e33b9bdbaf2aaa9cdf0345
joelgrod <joelgrod@gmail.com>
2021-03-18 15:46:54 +0000

    Reactive forms

--- src/app/reactive/pages/basics/basics.component.ts
+++ src/app/reactive/pages/basics/basics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -6,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent implements OnInit{
 
-  constructor() { }
+  // 1º Option
+  // public basics_form: FormGroup = new FormGroup({
+  //   'name': new FormControl('test'),
+  //   'price': new FormControl(0),
+  //   'stock': new FormControl(0),
+  // });
+  
+  // 2º Option: Better approach
+  public basics_form: FormGroup = this.fb.group({
+    name  : ['Name', [Validators.required, Validators.minLength(3)]],
+    price : [0 , [Validators.required, Validators.min(0)]],
+    stock : [0 , [Validators.required, Validators.min(0)]]
+  });
+  
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+  }
+
+  fieldNotValid(field: string) {
+    return this.basics_form.controls[field].errors && this.basics_form.controls[field].touched;
+  }
+
+  save(): void {
+
+    if(this.basics_form.invalid) {
+      this.basics_form.markAllAsTouched();
+      return;
+    };
+
+    console.log(this.basics_form.value);
+    this.basics_form.reset({
+      name: 'test',
+      price: 0,
+      stock: 0
+    });
   }
 
 }
--- src/app/reactive/pages/dinamics/dinamics.component.ts
+++ src/app/reactive/pages/dinamics/dinamics.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamics',
@@ -6,11 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
 
-  constructor() { }
+  public dinamics_form: FormGroup = this.fb.group({
+    name: [ , [Validators.required, Validators.minLength(3)]],  //Form control
+    favourites: this.fb.array([                                 // Form Controls Array
+      ['Baba'],                                                 // Form control
+      ['Oxygen not included'],
+      ['Cyberpunk2077']
+    ], Validators.required)
+  });
 
-  ngOnInit(): void {
+  public new_favourite: FormControl = this.fb.control('', Validators.required);
+
+  get fav_array() {
+    return this.dinamics_form.get('favourites') as FormArray;
+    // return this.dinamics_form.controls.favourites as FormArray;
   }
 
+  constructor(private fb: FormBuilder) { }
+  
+  // Create service for this kind of repetitive logic
+  public field_not_valid(field: string) {
+    return this.dinamics_form.controls[field].errors 
+    && this.dinamics_form.controls[field].touched;
+  }
+
+  add_favourite(): void {
+    if(this.new_favourite.invalid) return;
+
+    this.fav_array.push(this.fb.control(this.new_favourite.value, Validators.required));
+
+    this.new_favourite.reset();
+  }
+
+  save(): void {
+
+    if(this.dinamics_form.invalid) {
+      this.dinamics_form.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.dinamics_form.value);
+
+  }
 }
